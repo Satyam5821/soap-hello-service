a4907419c36b9956714d716b1454e0c20ba112a7
Satyam Laheri <satyam.laheri@mphasis.com>
2026-04-29 22:52:15 +0530

    Update HelloEndpoint.java

--- src/main/java/com/example/soapservice/endpoint/HelloEndpoint.java
+++ src/main/java/com/example/soapservice/endpoint/HelloEndpoint.java
@@ -35,6 +35,7 @@ import java.util.function.BiConsumer;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.StringUtils;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
