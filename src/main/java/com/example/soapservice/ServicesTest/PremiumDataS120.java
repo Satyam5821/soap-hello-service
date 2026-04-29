69da1ee2ff271357c0cd11a0a89ef28ac50dfce4
Satyam Laheri <satyam.laheri@mphasis.com>
2026-04-29 22:39:07 +0530

    fix build

---
+++ src/main/java/com/example/soapservice/ServicesTest/PremiumDataS120.java
@@ -0,0 +1,18 @@
+package com.example.soapservice.ServicesTest;
+
+/**
+ * Intentional test fixture for Sonar rule java:S120 (package naming regex).
+ * Not referenced by production code.
+ */
+public class PremiumDataS120 {
+  private int id;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+}
+
